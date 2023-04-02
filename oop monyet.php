2196c08e90f2e1bbb08a6ce37b5eff61feb0e3e3
YumnaFajar <129520312+YumnaFajar@users.noreply.github.com>
2023-04-02 22:05:19 +0700

    Add files via upload

---
+++ oop monyet.php
@@ -0,0 +1,35 @@
+<?php
+interface Hewan {
+    
+    function berjalan();
+    function makan();
+    function bersuara();
+}
+
+interface Monyet extends Hewan {
+    function berkendara();
+}
+
+class Dilatih implements Monyet {
+    
+    public function __construct() {
+    }
+    
+    function berjalan(){
+        echo "Berjalan";
+    }
+    function makan() {
+        echo "Makan";
+    }
+    function bersuara() {
+        echo "Bersuara";
+    }
+    function berkendara() {
+        echo "Berkendara";
+    }
+}
+
+$latih = new Dilatih();
+$latih-> berjalan()
+
+?>
